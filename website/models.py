4f583a06a9328fc6d81dcac7d2c2c9bfd9fba7f9
splopes <andrelopes.spdia@gmail.com>
2019-09-25 12:12:54 -0300

    criar lista para ver cadastrados

--- website/models.py
+++ website/models.py
@@ -8,5 +8,9 @@ class Pessoas(models.Model):
     cpf = models.CharField(max_length= 14, verbose_name='CPF')
     cep = models.CharField(max_length= 10, verbose_name='CEP')
     item_de_doacao = models.CharField(max_length=255, verbose_name='Item de doação')
+    
     ativo = models.BooleanField(default=True)
     data_criacao = models.DateTimeField(default=timezone.now, verbose_name='Data de criação')
+
+    def __str__(self):
+        return self.nome
--- website/views.py
+++ website/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
+from website.models import *
 
 def index(request):
+    if request.method == 'POST':
+        data = Pessoas()
+        data.nome = request.POST['nome']
+        data.sobrenome = request.POST['sobrenome']
+        data.data_de_nascimento = request.POST['data_de_nascimento']
+        data.cpf = request.POST['cpf']
+        data.cep = request.POST['cep']
+        data.item_de_doacao = request.POST['item_de_doacao']
+        data.save()
+
+        args = {
+            'sucesso': 'Você conseguiu se cadastar com sucesso!'
+        }
+
+        return render(request, 'index.html', args)
+
     return render(request, 'index.html')
+
+def listar_cadastrados(request):
+    listar_cadastrados = Pessoas.objects.all()
+    args = {
+        'listar_cadastrados': listar_cadastrados
+    }
+    return render(request, 'listar_cadastrados.html', args)
